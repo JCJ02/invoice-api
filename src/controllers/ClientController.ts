abaf78a01d1f6ed53501d7cb3e4d8bdf673bdf90
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-12 15:47:02 +0800

    Feat: Created Create, Update, Delete and List [w/ Search and Pagination]

---
+++ src/controllers/ClientController.ts
@@ -0,0 +1,345 @@
+import { Request, Response } from "express";
+import ClientService from "../services/ClientService";
+import AppResponse from "../utils/AppResponse";
+import { createClientSchema, createInvoicesArraySchema, updateClientSchema, updateInvoiceSchema } from "../utils/validations/ClientSchema";
+
+class ClientController {
+
+    private clientService;
+
+    constructor() {
+
+        this.clientService = new ClientService();
+
+        this.create = this.create.bind(this);
+        this.createMany = this.createMany.bind(this);
+        this.update = this.update.bind(this);
+        this.delete = this.delete.bind(this);
+        this.list = this.list.bind(this);
+        this.updateInvoice = this.updateInvoice.bind(this);
+        this.deleteInvoice = this.deleteInvoice.bind(this);
+        this.invoiceList = this.invoiceList.bind(this);
+
+    }
+
+    // CREATE CLIENT METHOD
+    async create(req: Request, res: Response) {
+        
+        try {
+
+            const validateClientData = createClientSchema.safeParse(req.body);
+
+            if(validateClientData.error) {
+                return AppResponse.sendErrors({
+                    res,
+                    data: null,
+                    message: validateClientData.error.errors[0].message,
+                    code: 403
+                });
+            } else {
+
+                const isClientDataValid = await this.clientService.create(validateClientData.data);
+
+                if(!isClientDataValid) {
+                    return AppResponse.sendErrors({
+                        res,
+                        data: null,
+                        message: "Invalid Credentials!",
+                        code: 403
+                    });
+                } else {
+                    return AppResponse.sendSuccessful({
+                        res,
+                        data: isClientDataValid,
+                        message: "Successfully Created!",
+                        code: 201
+                    });
+                }
+
+            }
+            
+        } catch (error: any) {
+            return AppResponse.sendErrors({
+                res,
+                data: null,
+                message: error.message,
+                code: 500
+            });
+        }
+
+    }
+
+    // CREATE INVOICES METHOD
+    async createMany(req: Request, res: Response) {
+        try {
+
+            const id = Number(req.params.id);
+            
+            const validation = createInvoicesArraySchema.safeParse(req.body.invoices);
+
+            if(validation.error) {
+                return AppResponse.sendErrors({
+                    res,
+                    data: null,
+                    message: validation.error.errors[0].message,
+                    code: 403
+                });
+            } else {
+
+                // const areInvoicesValid = await this.clientService.createMany(validation.data);
+
+                const areInvoicesValid = await this.clientService.createMany(id, validation.data);
+
+                if(!areInvoicesValid) {
+                    return AppResponse.sendErrors({
+                        res,
+                        data: null,
+                        message: "Invalid Inputs!",
+                        code: 403
+                    });
+                } else {
+                    return AppResponse.sendSuccessful({
+                        res,
+                        data: {
+                            invoices: areInvoicesValid
+                        },
+                        message: "Successfully Created!",
+                        code: 201
+                    });
+                }
+
+            }
+
+        } catch (error: any) {
+            return AppResponse.sendErrors({
+                res,
+                data: null,
+                message: error.message,
+                code: 500
+            });
+        }
+    }
+
+    // UPDATE CLIENT METHOD
+    async update(req: Request, res: Response) {
+
+        try {
+            
+            const clientId = Number(req.params.id);
+
+            const validateClientData = updateClientSchema.safeParse(req.body);
+
+            if(validateClientData.error) {
+                return AppResponse.sendErrors({
+                    res,
+                    data: null,
+                    message: validateClientData.error.errors[0].message,
+                    code: 403
+                });
+            } else {
+
+                const isClientUpdated = await this.clientService.update(clientId, validateClientData.data);
+
+                if(!isClientUpdated) {
+                    return AppResponse.sendErrors({
+                        res,
+                        data: null,
+                        message: "Failed To Update!",
+                        code: 403
+                    });
+                } else {
+                    return AppResponse.sendSuccessful({
+                        res,
+                        data: isClientUpdated,
+                        message: "Successfully Updated!",
+                        code: 201
+                    });
+                }
+
+            }
+
+        } catch (error: any) {
+            return AppResponse.sendErrors({
+                res,
+                data: null,
+                message: error.message,
+                code: 500
+            });
+        }
+
+    }
+
+    // DELETE CLIENT METHOD
+    async delete(req: Request, res: Response) {
+
+        try {
+
+            const clientId = Number(req.params.id);
+
+            const isClientDeleted = await this.clientService.delete(clientId);
+
+            if(!isClientDeleted) {
+                return AppResponse.sendErrors({
+                    res,
+                    data: null,
+                    message: "Failed To Delete!",
+                    code: 403
+                });
+            } else {
+                return AppResponse.sendSuccessful({
+                    res,
+                    data: null,
+                    message: "Successfully Deleted!",
+                    code: 200
+                });
+            }
+            
+        } catch (error: any) {
+            return AppResponse.sendErrors({
+                res,
+                data: null,
+                message: error.message,
+                code: 500
+            });
+        }
+
+    }
+
+    // CLIENT LIST w/ SEARCH AND PAGINATION
+    async list(req: Request, res: Response) {
+
+        try {
+
+            const searchResults = await this.clientService.list(req);
+
+            return AppResponse.sendSuccessful({
+                res,
+                data: searchResults,
+                message: "Result!",
+                code: 200
+            });
+            
+        } catch (error: any) {
+            return AppResponse.sendErrors({
+                res,
+                data: null,
+                message: error.message,
+                code: 500
+            });
+        }
+        
+    }
+
+    // UPDATE INVOICE METHOD
+    async updateInvoice(req: Request, res: Response) {
+
+        try {
+
+            const invoiceId = Number(req.params.id);
+
+            const validateInvoiceData = updateInvoiceSchema.safeParse(req.body);
+
+            if(validateInvoiceData.error) {
+                return AppResponse.sendErrors({
+                    res,
+                    data: null,
+                    message: validateInvoiceData.error.errors[0].message,
+                    code: 403
+                });
+            } else {
+
+                const isInvoiceUpdated = await this.clientService.updateInvoice(invoiceId, validateInvoiceData.data);
+
+                if(!isInvoiceUpdated) {
+                    return AppResponse.sendErrors({
+                        res,
+                        data: null,
+                        message: "Failed To Update!",
+                        code: 403
+                    });
+                } else {
+                    return AppResponse.sendSuccessful({
+                        res,
+                        data: isInvoiceUpdated,
+                        message: "Successfully Updated!",
+                        code: 201
+                    });
+                }
+
+            }
+            
+        } catch (error: any) {
+            return AppResponse.sendErrors({
+                res,
+                data: null,
+                message: error.message,
+                code: 500
+            });
+        }
+
+    }
+
+    // DELETE INVOICE METHOD
+    async deleteInvoice(req: Request, res: Response) {
+
+        try {
+
+            const invoiceId = Number(req.params.id);
+            
+            const isInvoiceDeleted = await this.clientService.deleteInvoice(invoiceId);
+
+            if(!isInvoiceDeleted) {
+                return AppResponse.sendErrors({
+                    res,
+                    data: null,
+                    message: "Failed To Delete!",
+                    code: 403
+                });
+            } else {
+                return AppResponse.sendSuccessful({
+                    res,
+                    data: null,
+                    message: "Successfully Deleted!",
+                    code: 200
+                });
+            }
+
+        } catch (error: any) {
+            return AppResponse.sendErrors({
+                res,
+                data: null,
+                message: error.message,
+                code: 500
+            });
+        }
+
+    }
+
+    // INVOICE LIST w/ SEARCH AND PAGINATION
+    async invoiceList(req: Request, res: Response) {
+
+        try {
+
+            const searchResults = await this.clientService.invoiceList(req);
+
+            return AppResponse.sendSuccessful({
+                res,
+                data: searchResults,
+                message: "Result!",
+                code: 200
+            });
+            
+        } catch (error: any) {
+            return AppResponse.sendErrors({
+                res,
+                data: null,
+                message: error.message,
+                code: 500
+            });
+        }
+
+    }
+
+}
+
+export default ClientController;
---
+++ src/repo/ClientRepo.ts
@@ -0,0 +1,492 @@
+import { clientType, invoiceType } from "../types/ClientTypes";
+import prisma from "../utils/client";
+
+class ClientRepo {
+
+    // CREATE CLIENT METHOD
+    async create(data: clientType) {
+    
+        const newClient = await prisma.$transaction(async (prisma) => {
+            return await prisma.client.create({
+                data: {
+                    firstname: data.firstname,
+                    lastname: data.lastname,
+                    companyName: data.companyName,
+                    email: data.email,
+                    phoneNumber: data.phoneNumber,
+                    businessPhone: data.businessPhone,
+                    mobilePhone: data.mobilePhone,
+                    address: data.address
+                }
+            });
+        });
+
+        return newClient;
+
+    }
+
+    // SHOW METHOD
+    async show(id: number) {
+
+        const client = await prisma.client.findFirst({
+            where: {
+                id: id,
+                deletedAt: null
+            }
+        });
+
+        return client;
+
+    }
+
+    // RETRIEVE INVOICE ID METHOD 
+    async retrieve(id: number) {
+
+        const invoice = await prisma.invoices.findFirst({
+            where: {
+                id: id,
+                deletedAt: null
+            }
+        });
+
+        return invoice;
+
+    }
+
+    // VALIDATE INVOICE NUMBER METHOD
+    async validateInvoiceNumber() {
+
+        const isInvoiceNumberExist = await prisma.invoices.findFirst({
+            where: {
+                deletedAt: null
+            },
+            orderBy: {
+                invoiceNumber: 'desc'
+            }
+        });
+    
+        return isInvoiceNumberExist;
+
+    }
+
+    // CREATE INVOICES METHOD
+    async createMany(id: number, data: any[]) {
+
+        const invoiceData = data.map((invoice) => ({
+            ...invoice,
+            clientId: id
+        }));
+        
+        const createManyInvoices = await prisma.invoices.createMany({
+          data: invoiceData,
+          skipDuplicates: true,  
+        });
+
+        return createManyInvoices;
+
+    }
+
+    // UPDATE CLIENT METHOD
+    async update(id: number, data: clientType) {
+
+        const updateClient = await prisma.client.update({
+            where: {
+                id: id,
+                deletedAt: null
+            },
+            data: {
+                firstname: data.firstname,
+                lastname: data.lastname,
+                companyName: data.companyName,
+                email: data.email,
+                phoneNumber: data.phoneNumber,
+                businessPhone: data.businessPhone,
+                mobilePhone: data.mobilePhone,
+                address: data.address
+            }
+        });
+
+        return updateClient;
+
+    }
+
+    // DELETE CLIENT METHOD
+    async delete(id: number) {
+
+        const deleteClient = await prisma.client.update({
+            where: {
+                id: id,
+                deletedAt: null
+            },
+            data: {
+                deletedAt: new Date(),
+                invoices: {
+                    updateMany: {
+                        where: {
+                            clientId: id,
+                            deletedAt: null
+                        }, 
+                        data: {
+                            deletedAt: new Date()
+                        }
+                    }
+                }
+            }
+        });
+
+        return deleteClient;
+
+    }
+
+    // CLIENT LIST w/ SEARCH AND PAGINATION
+    async list(query: string, skip: number, limit: number) {
+
+        const clients = await prisma.client.findMany({
+            skip: skip,
+            take: limit,
+            where: {
+                deletedAt: null,
+                OR: [
+                   {
+                        firstname: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                   },
+                   {
+                        lastname: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                   },
+                   {
+                        companyName: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                   },
+                   {
+                        email: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                   },
+                   {
+                        phoneNumber: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                   },
+                   {
+                        businessPhone: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                   },
+                   {
+                        mobilePhone: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                   },
+                   {
+                        address: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                   }
+                ]
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
+
+        const totalClients = await prisma.client.count({
+            where: {
+                deletedAt: null,
+                OR: [
+                    {
+                        firstname: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        lastname: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        companyName: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        email: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        phoneNumber: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        businessPhone: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        mobilePhone: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        address: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    }
+                ]
+            }
+        });
+
+        return {
+            clients,
+            totalClients,
+        };
+
+    }
+
+    // UPDATE INVOICE METHOD
+    async updateInvoice(id: number, data: invoiceType) {
+
+        const editInvoice = await prisma.invoices.update({
+            where: {
+                id: id,
+                deletedAt: null
+            },
+            data: {
+                description: data.description,
+                rate: data.rate,
+                quantity: data.quantity,
+                lineTotal: data.lineTotal,
+                dueDate: data.dueDate,
+                totalOutstanding: data.totalOutstanding
+            }
+        });
+
+        return editInvoice;
+
+    }
+
+    // DELETE INVOICE METHOD
+    async deleteInvoice(id: number) {
+
+        const removeInvoice = await prisma.invoices.update({
+            where: {
+                id: id,
+                deletedAt: null
+            },
+            data: {
+                deletedAt: new Date()
+            }
+        });
+
+        return removeInvoice;
+
+    }
+
+    // // INVOICE LIST w/ SEARCH AND PAGINATION
+    // async invoiceList(query: string, skip: number, limit: number) {
+
+    //     const clients = await prisma.client.findMany({
+    //         skip: skip,
+    //         take: limit,
+    //         where: {
+    //             deletedAt: null,
+    //             OR: [
+    //                 {
+    //                     companyName: {
+    //                         contains: query,
+    //                         mode: "insensitive"
+    //                     }
+    //                 },
+    //                 {
+    //                     invoices: {
+    //                         some: {
+    //                             description: {
+    //                                 contains: query,
+    //                                 mode: "insensitive"
+    //                             },
+    //                             totalOutstanding: isNaN(Number(query)) ? undefined : {
+    //                                 equals: Number(query)
+    //                             }
+    //                         }
+    //                     }
+    //                 },
+    //             ]
+    //         },
+    //         include: {
+    //             invoices: {
+    //                 where: {
+    //                     deletedAt: null
+    //                 },
+    //                 orderBy: {
+    //                     createdAt: 'desc'
+    //                 },
+    //                 select: {
+    //                     id: true,
+    //                     invoiceNumber: true,
+    //                     description: true,
+    //                     rate: true,
+    //                     quantity: true,
+    //                     totalOutstanding: true,
+    //                     dueDate: true,
+    //                     issuedDate: true,
+    //                     createdAt: true,
+    //                     updatedAt: true,
+    //                     deletedAt: true
+    //                 }
+    //             }
+    //         },
+    //         orderBy: {
+    //             createdAt: "desc"
+    //         }
+    //     });
+
+    //     const totalClients = await prisma.client.count({
+    //         where: {
+    //             deletedAt: null,
+    //             OR: [
+    //                 {
+    //                     companyName: {
+    //                         contains: query,
+    //                         mode: "insensitive"
+    //                     }
+    //                 },
+    //                 {
+    //                     invoices: {
+    //                         some: {
+    //                             description: {
+    //                                 contains: query,
+    //                                 mode: "insensitive"
+    //                             },
+    //                             totalOutstanding: isNaN(Number(query)) ? undefined : {
+    //                                 equals: Number(query)
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             ]
+    //         }
+    //     });
+
+    //     return {
+    //         clients,
+    //         totalClients,
+    //     };
+
+    // }
+
+    // INVOICE LIST w/ SEARCH AND PAGINATION
+    async invoiceList(query: string, skip: number, limit: number) {
+        const clients = await prisma.client.findMany({
+            skip: skip,
+            take: limit,
+            where: {
+                deletedAt: null,
+                OR: [
+                    {
+                        companyName: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        invoices: {
+                            some: {
+                                description: {
+                                    contains: query,
+                                    mode: "insensitive"
+                                },
+                                totalOutstanding: isNaN(Number(query)) ? undefined : {
+                                    equals: Number(query)
+                                }
+                            }
+                        }
+                    },
+                ]
+            },
+            include: {
+                invoices: {
+                    where: {
+                        deletedAt: null
+                    },
+                    orderBy: {
+                        createdAt: 'desc'
+                    },
+                    select: {
+                        id: true,
+                        invoiceNumber: true,
+                        description: true,
+                        rate: true,
+                        quantity: true,
+                        totalOutstanding: true,
+                        dueDate: true,
+                        issuedDate: true,
+                        createdAt: true,
+                        updatedAt: true,
+                        deletedAt: true
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
+
+        const totalClients = await prisma.client.count({
+            where: {
+                deletedAt: null,
+                OR: [
+                    {
+                        companyName: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        invoices: {
+                            some: {
+                                description: {
+                                    contains: query,
+                                    mode: "insensitive"
+                                },
+                                totalOutstanding: isNaN(Number(query)) ? undefined : {
+                                    equals: Number(query)
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        });
+
+        return {
+            clients,
+            totalClients,
+        };
+    }
+
+
+}
+
+export default ClientRepo;
