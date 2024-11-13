fbb867e6210b49a2b4803ffac16f03ef98341b96
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-13 11:36:40 +0800

    Fix: Routes

--- src/routes/ClientRoute.ts
+++ src/routes/ClientRoute.ts
@@ -5,15 +5,18 @@ import ClientController from "../controllers/ClientController";
 const clientRoute = express.Router();
 const clientController = new ClientController();
 
+// CLIENT ROUTES
 clientRoute.post("/", authMiddleware, clientController.create);
-clientRoute.post("/create-invoices/:id", authMiddleware, clientController.createMany);
 clientRoute.put("/:id", authMiddleware, clientController.update);
 clientRoute.delete("/:id", authMiddleware, clientController.delete);
 clientRoute.get("/", authMiddleware, clientController.list);
-clientRoute.put("/update-invoice/:id", clientController.updateInvoice);
-clientRoute.delete("/delete-invoice/:id", clientController.deleteInvoice);
-clientRoute.get("/invoice-list", authMiddleware, clientController.invoiceList);
-clientRoute.get("/:id", clientController.get);
-clientRoute.get("/retrive/:id", clientController.getInvoice);
+clientRoute.get("/:id", authMiddleware, clientController.get);
+
+// INVOICES ROUTES
+clientRoute.post("/create-invoices/:id", authMiddleware, clientController.createMany);
+clientRoute.put("/update-invoice/:id", authMiddleware, clientController.updateInvoice);
+clientRoute.delete("/delete-invoice/:id", authMiddleware, clientController.deleteInvoice);
+clientRoute.get("/retrive/invoices-list", authMiddleware, clientController.invoiceList);
+clientRoute.get("/retrive/:id", authMiddleware, clientController.getInvoice);
 
 export default clientRoute;
