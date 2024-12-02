0683cda12b7bcea8faffa04b1292f53932fd18db
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-12-02 15:19:24 +0800

    Fix: Adjust the Requirements

--- src/utils/validations/ClientSchema.ts
+++ src/utils/validations/ClientSchema.ts
@@ -1,15 +1,13 @@
 import { z } from "zod";
 
 const createClientSchema = z.object({
-    firstname: z.string({
-        required_error: "Firstname Is Required!",
-        invalid_type_error: "Firstname Must Be String!"
-    }),
-    lastname: z.string({
-        required_error: "Lastname Is Required!",
-        invalid_type_error: "Lastname Must Be String!"
-    }),
-    email: z.string().email("Must Be A Valid E-mail!").optional().nullable(),
+    firstname: z.string().min(1, "Firstname Is Required!")
+        .max(255, "Firstname Must Not Exceed To 255 Characters"),
+    lastname: z.string().min(1, "Lastname Is Required!")
+        .max(255, "Lastname Must Not Exceed To 255 Characters"),
+    email: z.string().refine(value => value === "" || /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value), {
+        message: "Must Be A Valid E-mail!",
+      }).optional().nullable(),
     companyName: z.string({
         required_error: "Company Name Is Required!",
         invalid_type_error: "Company Name Must Be String!"
