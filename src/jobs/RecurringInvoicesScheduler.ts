6f238c2278f2ab77fb6930c90e4046f19eba2009
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-03-28 01:22:28 +0800

    Feat: Refactor

--- src/jobs/RecurringInvoicesScheduler.ts
+++ src/jobs/RecurringInvoicesScheduler.ts
@@ -3,7 +3,7 @@ import ClientService from '../services/ClientService';
 
 //INITIALIZE DEPENDECIES APPROPRIATELY
 const clientService = new ClientService();
-
+// RUNS EVERY 10 SECONDS = "*/10 * * * * *"
 // RUNS AT 12:00 AM (MIDNIGHT) UTC EVERY DAY.
 cron.schedule("0 0 * * *", async () => {
     console.log("Running Daily Invoice Generation...");
